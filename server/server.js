5f78e5668eb7d336c77f3061a04eb6c78c7e8788
tuchaunutuyetminh <tuchaunutuyetminh@gmail.com>
2023-12-08 21:40:31 +0700

    #2 Handle error & hash password

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 const  express = require('express')
 require('dotenv').config()
+const dbConnect = require('./config/dbconnect')
+const initRoutes = require('./routes')
+
 
 const app = express()
 const port = process.env.PORT || 8888 
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-
-app.use('/', (req, res) => { res.send('SERVER ONNN')})
+app.use(express.urlencoded({ extended: true }))
+dbConnect()
+initRoutes(app)
 
 app.listen(port, () => {
     console.log('server running on the port: ' + port)
