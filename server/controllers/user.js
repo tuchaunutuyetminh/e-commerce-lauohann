57be5057f33c8174ab561ed25bb0a65dbd01b372
tuchaunutuyetminh <tuchaunutuyetminh@gmail.com>
2023-12-09 20:44:09 +0700

    #3 logic login

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -8,13 +8,42 @@ const register = asyncHandler(async (req, res) => {
         success: false,
         mes: 'Missing inputs'
     })
-    const response = await User.create(req.body)
-    return res.status(200).json({
-        success: response ? true : false,
-        response
-    })
+
+    const user = await User.findOne({ email })
+    if(user) throw new Error('User has existed!')
+    else {
+        const newUser = await User.create(req.body)
+        return res.status(200).json({
+            success: newUser ? true : false,
+            mes: newUser ? 'Register is successfully. Please go login~' : 'Something went wrong'
+        })
+    }
+    
+})
+
+
+const login = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+    if(!email || !password)
+        return res.status(400).json({
+            success: false,
+            mes: 'Missing inputs'
+        })
+
+    const response = await User.findOne({ email })
+    if(response && await response.isCorrectPassword(password)) {
+        const { password, role, ...userData } = response.toObject()
+        return res.status(200).json({
+            success: true,
+            userData
+        })
+    } else {
+        throw new Error('Invalid credentials!')
+    }
+    
 })
 
 module.exports = {
-    register
+    register,
+    login
 }
