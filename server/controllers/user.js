9b9af30df2522d20c94824d7db95af9c9bf33a96
tuchaunutuyetminh <tuchaunutuyetminh@gmail.com>
2023-12-10 21:03:13 +0700

    #4 access token & refresh token

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,6 @@
 const User = require('../models/user')
 const asyncHandler = require('express-async-handler')
+const { generateAccessToken, generateRefreshToken } = require('../middewares/jwt')
 
 const register = asyncHandler(async (req, res) => {
     const {email, password, firstname, lastname} = req.body
@@ -21,7 +22,8 @@ const register = asyncHandler(async (req, res) => {
     
 })
 
-
+//Refresh token => cấp mới access token
+//Access token => xác thực người dùng, phân quyền người dùng 
 const login = asyncHandler(async (req, res) => {
     const { email, password } = req.body
     if(!email || !password)
@@ -32,9 +34,23 @@ const login = asyncHandler(async (req, res) => {
 
     const response = await User.findOne({ email })
     if(response && await response.isCorrectPassword(password)) {
+        //Tách password và role ra khỏi response
         const { password, role, ...userData } = response.toObject()
+
+        //Tạo accesc token
+        const accessToken = generateAccessToken(response._id, role)
+
+        //Tạo refresh token
+        const refreshToken = generateRefreshToken(response._id)
+
+        // Lưu refresh token vào database 
+        await User.findByIdAndUpdate(response._id, { refreshToken }, { new: true})
+        //Lưu refresh token vào cookie
+        res.cookie('refreshToken', refreshToken, {httpOnly: true, maxAge: 7 * 24 * 60 * 60 * 1000 })
+        
         return res.status(200).json({
             success: true,
+            accessToken,
             userData
         })
     } else {
@@ -43,7 +59,18 @@ const login = asyncHandler(async (req, res) => {
     
 })
 
+
+const getCurrent = asyncHandler(async (req, res) => {
+    const { _id } = req.user
+    const user = await User.findById(_id).select('-refreshToken -password -role')
+    return res.status(200).json({
+        success: false,
+        rs: user ? user : 'User not found'
+    })
+    
+})
 module.exports = {
     register,
-    login
+    login,
+    getCurrent
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,12 @@
 const router = require('express').Router()
 const ctrls = require('../controllers/user')
+const { verifyAccessToken } = require('../middewares/verifyToken')
+
 
 router.post('/register', ctrls.register)
 router.post('/login', ctrls.login)
+router.get('/current', verifyAccessToken, ctrls.getCurrent)
+
 
 module.exports = router
 
