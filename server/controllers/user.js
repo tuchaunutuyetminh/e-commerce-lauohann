84513a5985b176c9e16c2059ce775b2d62240b00
tuchaunutuyetminh <tuchaunutuyetminh@gmail.com>
2023-12-14 21:20:01 +0700

    #4 CRUD user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -39,18 +39,18 @@ const login = asyncHandler(async (req, res) => {
     const response = await User.findOne({ email })
     if(response && await response.isCorrectPassword(password)) {
         //Tách password và role ra khỏi response
-        const { password, role, ...userData } = response.toObject()
+        const { password, role,refreshToken, ...userData } = response.toObject()
 
         //Tạo accesc token
         const accessToken = generateAccessToken(response._id, role)
 
         //Tạo refresh token
-        const refreshToken = generateRefreshToken(response._id)
+        const newrefreshToken = generateRefreshToken(response._id)
 
         // Lưu refresh token vào database 
-        await User.findByIdAndUpdate(response._id, { refreshToken }, { new: true})
+        await User.findByIdAndUpdate(response._id, { refreshToken: newrefreshToken }, { new: true})
         //Lưu refresh token vào cookie
-        res.cookie('refreshToken', refreshToken, {httpOnly: true, maxAge: 7 * 24 * 60 * 60 * 1000 })
+        res.cookie('refreshToken', newrefreshToken, {httpOnly: true, maxAge: 7 * 24 * 60 * 60 * 1000 })
         
         return res.status(200).json({
             success: true,
@@ -153,6 +153,45 @@ const resetPassword = asyncHandler(async(req, res,) => {
         mes: user ? 'Update password' : 'Something went wrongf'
     })
 })
+
+const getUsers = asyncHandler(async(req, res) => {
+    const response = await User.find().select('-refreshToken -password -role')
+    return res.status(200).json({
+        success: response ? true : false,
+        users: response
+    })
+})
+
+const deleteUser = asyncHandler(async(req, res) => {
+    const { _id } = req.query
+    if(!_id) throw new Error('Mising inputs')
+    const response = await User.findByIdAndDelete(_id)
+    return res.status(200).json({
+        success: response ? true : false,
+        deletedUser: response ? `User with email ${response.email} deleted` : 'No user delete'
+    })
+})
+
+const updateUser = asyncHandler(async(req, res) => {
+    const { _id } = req.user
+    if(!_id || Object.keys(req.body).length === 0) throw new Error('Mising inputs')
+    const response = await User.findByIdAndUpdate(_id, req.body, {new: true}).select('-password -role')
+    return res.status(200).json({
+        success: response ? true : false,
+        updatedUser: response ? response : 'Something went wrong'
+    })
+})
+
+const updateUserByAdmin = asyncHandler(async(req, res) => {
+    const { uid } = req.params
+    if(Object.keys(req.body).length === 0) throw new Error('Mising inputs')
+    const response = await User.findByIdAndUpdate(uid, req.body, {new: true}).select('-password -role')
+    return res.status(200).json({
+        success: response ? true : false,
+        updatedUser: response ? response : 'Something went wrong'
+    })
+})
+
 module.exports = {
     register,
     login,
@@ -160,5 +199,9 @@ module.exports = {
     refreshAccessToken,
     logout,
     forgotPassword,
-    resetPassword
+    resetPassword,
+    getUsers,
+    deleteUser,
+    updateUser,
+    updateUserByAdmin
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const ctrls = require('../controllers/user')
-const { verifyAccessToken } = require('../middewares/verifyToken')
+const { verifyAccessToken, isAdmin } = require('../middewares/verifyToken')
 
 
 router.post('/register', ctrls.register)
@@ -10,6 +10,11 @@ router.post('/refreshToken', ctrls.refreshAccessToken)
 router.get('/logout', ctrls.logout)
 router.get('/forgotpassword', ctrls.forgotPassword)
 router.put('/resetpassword', ctrls.resetPassword)
+router.get('/',[verifyAccessToken, isAdmin], ctrls.getUsers)
+router.delete('/',[verifyAccessToken, isAdmin], ctrls.deleteUser)
+router.put('/current',verifyAccessToken, ctrls.updateUser)
+router.put('/:uid',[verifyAccessToken, isAdmin], ctrls.updateUserByAdmin)
+
 
 
 
